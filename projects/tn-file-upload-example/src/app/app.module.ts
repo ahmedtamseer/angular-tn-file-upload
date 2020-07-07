76bd270955ea6c9df341e02c7295d023af4fd581
ahmedtamseer <ahmedtamseer3@gmail.com>
2020-07-08 00:33:22 +0530

    fixed issue in hidden type

--- projects/tn-file-upload-example/src/app/app.module.ts
+++ projects/tn-file-upload-example/src/app/app.module.ts
@@ -1,16 +1,14 @@
+// import { TnFileUploadModule } from '@ahmedtamseer/tn-file-upload';
 import { TnFileUploadModule } from 'tn-file-upload';
-// import { TnFileUploadModule } from './lib/tn-file-upload.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-// import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    // TnFileUploadComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
